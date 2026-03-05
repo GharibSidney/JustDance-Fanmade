b1374af871f9f61a523633d63f78c88011a5a08e
GharibSidney <sidney-gharib@hotmail.com>
2026-03-05 12:10:40 -0500

    added back the normal download of youtube music

--- download_youtube_video.py
+++ download_youtube_video.py
@@ -1,9 +1,22 @@
+# import yt_dlp
+
+# url = "https://www.youtube.com/watch?v=NJh5idlanrc"
+
+# ydl_opts = {
+#     'format': 'bestvideo[height<=360]+bestaudio/best[height<=360]',
+#     'outtmpl': 'downloads/%(title)s.%(ext)s',
+# }
+
+# with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+#     ydl.download([url])
+
+
+# Normal format
 import yt_dlp
 
 url = "https://www.youtube.com/watch?v=NJh5idlanrc"
 
 ydl_opts = {
-    'format': 'bestvideo[height<=360]+bestaudio/best[height<=360]',
     'outtmpl': 'downloads/%(title)s.%(ext)s',
 }
 
